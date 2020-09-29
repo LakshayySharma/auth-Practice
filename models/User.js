9d7dff0fc29bb3e6f72f7c7ccae3efaf6c1a7983
Lakshay <ls199575@gmail.com>
2020-09-29 13:15:26 +0530

    model created

---
+++ models/User.js
@@ -0,0 +1,19 @@
+const db = require('../config/db');
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name: { 
+        type: String, 
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = User = mongoose.model('user', userSchema);
